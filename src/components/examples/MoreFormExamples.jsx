0e9235fd4ee409fc4bcaf7630ae3c6921e606676
Mohammed Cherbatji <cherbatjim@iadt.ie>
2025-11-27 14:05:01 +0000

    Added API data, enums, calendar example in forms

--- src/components/examples/MoreFormExamples.jsx
+++ src/components/examples/MoreFormExamples.jsx
@@ -1,4 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "@/config/api";
+
+import { useForm, Controller } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -10,7 +15,12 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Field, FieldDescription, FieldLabel } from "@/components/ui/field";
+import {
+  Field,
+  FieldDescription,
+  FieldLabel,
+  FieldError,
+} from "@/components/ui/field";
 import {
   Select,
   SelectContent,
@@ -29,11 +39,59 @@ import { ChevronDownIcon } from "lucide-react";
 
 export default function MoreFormExamples() {
   const [dobWindowOpen, setDobWindowOpen] = useState(false);
-  const [dob, setDob] = useState(null);
+  const [performers, setPerformers] = useState([]);
 
-  const submitForm = (e) => {
-    e.preventDefault();
-    console.log("Form submitted");
+  useEffect(() => {
+    const fetchPerformers = async () => {
+      const options = {
+        method: "GET",
+        url: "/performers",
+      };
+
+      try {
+        let response = await axios.request(options);
+        console.log(response.data);
+        setPerformers(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchPerformers();
+  }, []);
+
+  const formSchema = z.object({
+    department: z.enum([
+      "Engineering",
+      "Design",
+      "Marketing",
+      "Sales",
+      "Customer Support",
+    ]),
+    dob: z.date("Date of Birth is required"),
+    performer: z.string().min(1, "Performer is required"),
+  });
+
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      department: "",
+      dob: "",
+      performer: "",
+    },
+    mode: "onChange",
+  });
+
+  const submitForm = (data) => {
+    console.log(data);
+
+    let formattedData = {
+      ...data,
+      dob: data.dob.toISOString().split("T")[0],
+      performer_id: data.performer,
+    };
+    console.log("Formatted Data:");
+    console.log(formattedData);
   };
 
   return (
@@ -42,79 +100,119 @@ export default function MoreFormExamples() {
         <CardTitle>Various Form Examples</CardTitle>
       </CardHeader>
       <CardContent>
-        <form id="form-demo-2" onSubmit={submitForm}>
+        <form id="form-demo-2" onSubmit={form.handleSubmit(submitForm)}>
           <div className="flex flex-col gap-6">
-            <Field>
-              <FieldLabel>Department (Enums)</FieldLabel>
-              <Select>
-                <SelectTrigger>
-                  <SelectValue placeholder="Choose department" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="engineering">Engineering</SelectItem>
-                  <SelectItem value="design">Design</SelectItem>
-                  <SelectItem value="marketing">Marketing</SelectItem>
-                  <SelectItem value="sales">Sales</SelectItem>
-                  <SelectItem value="support">Customer Support</SelectItem>
-                </SelectContent>
-              </Select>
-              <FieldDescription>
-                Select your department or area of work.
-              </FieldDescription>
-            </Field>
-
-            <Field>
-              <FieldLabel>Select Performer (API data)</FieldLabel>
-              <Select>
-                <SelectTrigger>
-                  <SelectValue placeholder="Choose performer" />
-                </SelectTrigger>
-                <SelectContent>
-                  {/* <SelectItem value=""></SelectItem> */}
-                </SelectContent>
-              </Select>
-              <FieldDescription>Select your performer.</FieldDescription>
-            </Field>
-
-            <Field>
-              <FieldLabel>Date of Birth</FieldLabel>
-              <Popover open={dobWindowOpen} onOpenChange={setDobWindowOpen}>
-                <PopoverTrigger asChild>
-                  <Button
-                    variant="outline"
-                    id="date"
-                    className="w-48 justify-between font-normal"
+            <Controller
+              name="department"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel>Department (Enums)</FieldLabel>
+                  <Select
+                    name={field.name}
+                    onValueChange={field.onChange}
+                    value={field.value}
                   >
-                    {dob ? dob.toLocaleDateString() : "Select date"}
-                    <ChevronDownIcon />
-                  </Button>
-                </PopoverTrigger>
-                <PopoverContent
-                  className="w-auto overflow-hidden p-0"
-                  align="start"
-                >
-                  <Calendar
-                    mode="single"
-                    selected={dob}
-                    captionLayout="dropdown"
-                    onSelect={(selectedDate) => {
-                      setDob(selectedDate);
-                      setDobWindowOpen(false);
-                    }}
-                  />
-                </PopoverContent>
-              </Popover>
-              <FieldDescription>Select your dob.</FieldDescription>
-            </Field>
+                    <SelectTrigger aria-invalid={fieldState.invalid}>
+                      <SelectValue placeholder="Choose department" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="Engineering">Engineering</SelectItem>
+                      <SelectItem value="Design">Design</SelectItem>
+                      <SelectItem value="Marketing">Marketing</SelectItem>
+                      <SelectItem value="Sales">Sales</SelectItem>
+                      <SelectItem value="Customer Support">
+                        Customer Support
+                      </SelectItem>
+                    </SelectContent>
+                  </Select>
+                  <FieldDescription>
+                    Select your department or area of work.
+                  </FieldDescription>
+                </Field>
+              )}
+            />
+
+            <Controller
+              name="performer"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel>Select Performer (API data)</FieldLabel>
+                  <Select
+                    name={field.name}
+                    onValueChange={field.onChange}
+                    value={field.value}
+                  >
+                    <SelectTrigger aria-invalid={fieldState.invalid}>
+                      <SelectValue placeholder="Choose performer" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {performers.map((performer) => (
+                        <SelectItem key={performer.id} value={performer.id.toString()}>
+                          {performer.title}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  <FieldDescription>Select your performer.</FieldDescription>
+                </Field>
+              )}
+            />
+
+            <Controller
+              name="dob"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="form-example-dob">
+                    Date of Birth
+                  </FieldLabel>
+                  <Popover open={dobWindowOpen} onOpenChange={setDobWindowOpen}>
+                    <PopoverTrigger asChild>
+                      <Button
+                        variant="outline"
+                        id="date"
+                        className="w-48 justify-between font-normal"
+                      >
+                        {field.value
+                          ? field.value.toLocaleDateString()
+                          : "Select date"}
+                        <ChevronDownIcon />
+                      </Button>
+                    </PopoverTrigger>
+                    <PopoverContent
+                      className="w-auto overflow-hidden p-0"
+                      align="start"
+                    >
+                      <Calendar
+                        mode="single"
+                        selected={field.value}
+                        captionLayout="dropdown"
+                        onSelect={(selectedDate) => {
+                          field.onChange(selectedDate);
+                          setDobWindowOpen(false);
+                        }}
+                      />
+                    </PopoverContent>
+                  </Popover>
+                  <FieldDescription>Select your dob.</FieldDescription>
+
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
           </div>
         </form>
       </CardContent>
       <CardFooter className="flex-col gap-2">
         <Field orientation="horizontal">
-          <Button type="button" variant="outline" onClick={() => {}}>
+          <Button type="button" variant="outline" onClick={() => form.reset()}>
             Reset
           </Button>
-          <Button type="submit" form="form-demo-2">
+          <Button variant="outline" type="submit" form="form-demo-2">
             Submit
           </Button>
         </Field>
